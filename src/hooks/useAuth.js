44d75195a0cb89f98d7f20a26bbe9c2f63cc0549
Skripnuyk <walker8613@gmail.com>
2023-06-08 20:35:11 +0300

    Додав хуки

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import { selectIsLoading, selectIsLoggedIn, selectIsRefreshing, selectUser } from "redux/auth/selectors";
+
+export const useAuth = () => {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+    const isRefreshing = useSelector(selectIsRefreshing);
+    const user = useSelector(selectUser);
+    const isLoading = useSelector(selectIsLoading);
+
+    return {
+        isLoggedIn,
+        isRefreshing,
+        user,
+        isLoading,
+    };
+};
